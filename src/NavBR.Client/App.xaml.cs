4fc7adc9171dfe166aee124565c9482139ff87f1
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-14 09:18:51 -0300

    feat(i18n): initialize culture at application startup

--- src/NavBR.Client/App.xaml.cs
+++ src/NavBR.Client/App.xaml.cs
@@ -1,7 +1,13 @@
 using System.Windows;
+using NavBR.Client.Localization;
 
 namespace NavBR.Client;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        LocalizationService.Initialize();
+        base.OnStartup(e);
+    }
 }
