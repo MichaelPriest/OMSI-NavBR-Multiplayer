54563c9891ba8364d3bc9e3c525c1d91ccc7c16d
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-14 13:12:22 -0300

    multiplayer: registrar estado compartilhado das salas

--- src/NavBR.Server/Program.cs
+++ src/NavBR.Server/Program.cs
@@ -1,8 +1,14 @@
 using NavBR.Server.Hubs;
+using NavBR.Server.Multiplayer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR();
+builder.Services.AddSingleton<MultiplayerRoomRegistry>();
+builder.Services.AddSignalR(options =>
+{
+    options.MaximumReceiveMessageSize = 64 * 1024;
+    options.EnableDetailedErrors = false;
+});
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -15,7 +21,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors();
-app.MapGet("/health", () => Results.Ok(new { status = "ok", service = "NavBR.Server" }));
+app.MapGet("/health", () => Results.Ok(new
+{
+    status = "ok",
+    service = "NavBR.Server",
+    multiplayer = "signalr"
+}));
 app.MapHub<MultiplayerHub>("/hubs/multiplayer");
 
 app.Run();
