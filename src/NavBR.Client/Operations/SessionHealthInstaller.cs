93d182d2899bb325ef3c88cd2e33962b9d510e7e
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-17 08:07:13 -0300

    refactor(alpha12): place session health in system area

--- src/NavBR.Client/Operations/SessionHealthInstaller.cs
+++ src/NavBR.Client/Operations/SessionHealthInstaller.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using NavBR.Client.Localization;
+using NavBR.Client.Windows;
 
 namespace NavBR.Client.Operations;
 
@@ -51,6 +52,12 @@ internal static class SessionHealthInstaller
                 window.GetCurrentTelemetryForAlpha11,
                 app.PluginBridge.GetConnectionInfo).ShowDialog();
         };
+
+        if (window.FindName(Alpha12ProfessionalShellInstaller.SystemPanelName) is Panel system)
+        {
+            system.Children.Add(button);
+            return button;
+        }
 
         var dispatcherButton = FindButtons(window).FirstOrDefault(candidate =>
             candidate.Tag as string == "alpha12-dispatcher");
