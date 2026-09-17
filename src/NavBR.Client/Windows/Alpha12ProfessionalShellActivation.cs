94e101e7b7cc40931e71e2173692f0505757b2a1
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-17 08:07:45 -0300

    chore(alpha12): remove shell activation marker

--- src/NavBR.Client/Windows/Alpha12ProfessionalShellActivation.cs
+++
@@ -1,7 +0,0 @@
-namespace NavBR.Client.Windows;
-
-// Marker type used by Alpha.12 diagnostics to identify the professional shell build.
-internal static class Alpha12ProfessionalShellActivation
-{
-    internal const string Version = "1";
-}
