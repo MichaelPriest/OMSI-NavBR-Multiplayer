0baf0575cbb163563d8b5f8438667dc425fbd7b6
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-17 11:12:00 -0300

    feat(alpha12): activate Figma preview shell

--- src/NavBR.Client/App.xaml.cs
+++ src/NavBR.Client/App.xaml.cs
@@ -125,7 +125,7 @@ public partial class App : Application
 
         if (window is MainWindow mainWindow)
         {
-            Alpha12ProfessionalShellInstaller.Install(mainWindow);
+            Alpha12FigmaShellInstaller.Install(mainWindow);
             Alpha11VisualTuning.Apply(mainWindow);
             OmsiProfilesUiInstaller.Install(mainWindow);
             Alpha12TechnicalControlsOrganizer.Attach(mainWindow);
