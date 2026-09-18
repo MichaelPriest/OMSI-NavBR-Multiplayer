741601ab2a57ccce2dc02d5132beeaa4a0e58304
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-18 20:33:00 -0300

    Server: support Render PORT binding [skip ci]

--- src/NavBR.Server/Program.cs
+++ src/NavBR.Server/Program.cs
@@ -1,4 +1,9 @@
 using NavBR.Server;
 
-var app = NavBRServerApplication.Build(args);
+var renderPort = Environment.GetEnvironmentVariable("PORT");
+var listenUrl = int.TryParse(renderPort, out var port) && port is > 0 and <= 65535
+    ? $"http://0.0.0.0:{port}"
+    : null;
+
+var app = NavBRServerApplication.Build(args, listenUrl);
 await app.RunAsync();
