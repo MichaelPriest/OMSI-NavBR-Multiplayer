a3435594c0b784a8c5178b9ed20f664a7f208334
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-18 14:13:30 -0300

    refactor(alpha14): open company team tab directly in React

--- src/NavBR.Client/Network/CompanyMembersWindow.cs
+++ src/NavBR.Client/Network/CompanyMembersWindow.cs
@@ -23,7 +23,7 @@ internal static class CompanyMembersInstaller
         var button = new Button { Tag = ButtonTag };
         StyleButton(button);
         ApplyLocalization(button);
-        button.Click += (_, _) => window.NavigatePrimaryWebShell("companyNetwork");
+        button.Click += (_, _) => window.NavigatePrimaryWebShell("companyNetwork-team");
         panel.Children.Add(button);
 
         SelectionChangedEventHandler languageChanged = (_, _) => ApplyLocalization(button);
