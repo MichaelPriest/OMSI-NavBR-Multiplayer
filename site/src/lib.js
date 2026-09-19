893e05182dd705e121541d4b53c5fb5f44ade2ff
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-09-19 01:17:51 -0300

    Site: point public downloads to Alpha.14 Test 5

--- site/src/lib.js
+++ site/src/lib.js
@@ -1,5 +1,5 @@
 export const REPO = "MichaelPriest/OMSI-NavBR-Multiplayer";
-export const CURRENT_TAG = "v0.3.0-alpha.14-test.4";
+export const CURRENT_TAG = "v0.3.0-alpha.14-test.5";
 export const GITHUB_URL = `https://github.com/${REPO}`;
 export const RELEASES_PAGE = `${GITHUB_URL}/releases`;
 
